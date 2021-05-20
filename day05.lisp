d423559ff9bc9a3762023a33e4dd156f6e18385d
Franck Yvonnet <franck@yvonnet.fr>
2021-05-20 07:41:44 +0200

    part 2

--- day05.lisp
+++ day05.lisp
@@ -1,25 +1,46 @@
 (defpackage :day05
   (:use :cl :aoc-misc)
   (:import-from :sb-md5 :md5sum-string)
+  (:import-from :serapeum :nlet)
   (:export main))
 
 (in-package :day05)
 
-(defun find-password (id &optional (index 0) (n 8))
-  (unless (zerop n)
-    (let ((md5sum (md5sum-string (format nil "~a~a" id index))))
-      (if
-        (and
-          (zerop (aref md5sum 0))
-          (zerop (aref md5sum 1))
-          (< (aref md5sum 2) 16))
-        (cons
-          (char "0123456789abcdef" (aref md5sum 2))
-          (find-password id (1+ index) (1- n)))
-        (find-password id (1+ index) n)))))
+(defun hexa-char (n)
+  (char "0123456789abcdef" n))
+
+(defun find-password (id)
+  (let
+    ((passwd1 (make-string 8 :initial-element #\#))
+     (passwd2 (make-string 8 :initial-element #\#)))
+    (nlet rec ((index 0) (n1 8) (n2 8))
+      (if (zerop n2)
+        (list passwd1 passwd2)
+        ; md5sum-string returns the MD5 hash as an array of 8-bit integers
+        (let ((md5sum (md5sum-string (format nil "~a~a" id index))))
+          (if
+            (and
+              (zerop (aref md5sum 0))  ; two first characters are zero
+              (zerop (aref md5sum 1))  ; two next characters are zero
+              (< (aref md5sum 2) 16))  ; 5th character is zero
+            (let 
+              ((a (aref md5sum 2))              ; 6th character
+               (b (floor (aref md5sum 3) 16)))  ; 7th character
+              (rec
+                (1+ index)
+                (if (zerop n1)
+                  n1
+                  (progn
+                    (setf (aref passwd1 (- 8 n1)) (hexa-char a))
+                    (1- n1)))
+                (if (and (< a 8) (char= (aref passwd2 a) #\#))
+                  (progn
+                    (setf (aref passwd2 a) (hexa-char b))
+                    (1- n2))
+                  n2)))
+            (rec (1+ index) n1 n2)))))))
 
 (defun main ()
-  (let
-    ((input (car (read-input-as-list 05))))
-    (format t "~a~%" (coerce (find-password input) 'string))))
+  (dolist (p (find-password (car (read-input-as-list 05))))
+    (format t "~a~%" p)))
 
