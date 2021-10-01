4ef0cf21dc8a0d4597d6f7f730df8721477f047b
Franck YVONNET <franck@yvonnet.fr>
2021-10-01 13:42:07 +0200

    Part 1 rewritten with Josephus algorithm

--- day19.lisp
+++ day19.lisp
@@ -1,29 +1,18 @@
 (defpackage :day19
   (:use :cl :aoc-misc)
-  (:import-from :functional-queue :empty-queue :queue-empty-p :queue-head :queue-tail :queue-snoc)
   (:import-from :serapeum :nlet)
   (:export main))
 
 (in-package :day19)
 
-(defun myfunc (queue)
-  (if (queue-empty-p (queue-tail queue))
-    (1+ (queue-head queue))
-    (myfunc
-      (queue-snoc
-        (queue-tail
-          (queue-tail
-            queue))
-        (queue-head queue)))))
+(defun josephus (n k)
+  (nlet rec ((a 1) (m 1))
+    (if (> a n)
+      (1+ m)
+      (rec (1+ a) (mod (+ m k) a)))))
 
 (defun main ()
   (let*
-    ((input (car (read-input-as-list 19 #'parse-integer)))
-     (queue
-       (nlet rec ((q (empty-queue)) (i 0))
-         ;(if (= i 5)
-         (if (= i input)
-           q
-           (rec (queue-snoc q i) (1+ i))))))
-    (print (myfunc queue))))
+    ( (input (car (read-input-as-list 19 #'parse-integer))))
+    (print (josephus input 2))))
 
