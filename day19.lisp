3cc6c2d6d2b93d224bf7b15d0da977ec76341488
Franck YVONNET <franck@yvonnet.fr>
2021-10-02 09:24:30 +0200

    part 1 remade with Numberphile's Josephus

--- day19.lisp
+++ day19.lisp
@@ -1,18 +1,18 @@
 (defpackage :day19
   (:use :cl :aoc-misc)
-  (:import-from :serapeum :nlet)
   (:export main))
 
 (in-package :day19)
 
-(defun josephus (n k)
-  (nlet rec ((a 1) (m 1))
-    (if (> a n)
-      (1+ m)
-      (rec (1+ a) (mod (+ m k) a)))))
+; https://www.youtube.com/watch?v=uCsD3ZGzMgE
+(defun josephus (n &optional (l 0) (bit-value 1))
+  (if (= n 1)
+    (1+ (* 2 l))
+    (multiple-value-bind (q r) (floor n 2)
+      (josephus q (+ l (* r bit-value)) (* 2 bit-value)))))
 
 (defun main ()
   (let*
     ( (input (car (read-input-as-list 19 #'parse-integer))))
-    (print (josephus input 2))))
+    (print (josephus input))))
 
