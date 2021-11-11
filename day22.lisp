0051e3bfe7fc44ac023970d7c61100ed1a6a89d8
Franck YVONNET <franck@yvonnet.fr>
2021-11-11 10:08:32 +0100

    part 2

--- day22.lisp
+++ day22.lisp
@@ -6,11 +6,18 @@
 
 (in-package :day22)
 
-(defun decode (line)
-  (multiple-value-bind (match regs) (scan-to-strings "^\/dev\/grid\/node-x(\\d+)-y(\\d+)\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)T\\s+\\d+\%$" line)
-    (when match
+(defun extract-data (lines &optional (output nil) (empty-node nil) (hole-in-wall nil) (x-max 0))
+  (multiple-value-bind (_ regs) (scan-to-strings "^\/dev\/grid\/node-x(\\d+)-y(\\d+)\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)T\\s+\\d+\%$" (car lines))
+    (if (null lines)
+      (list output empty-node hole-in-wall x-max)
       (destructuring-bind (x y size used avail) (map 'list #'parse-integer regs)
-        (list (make-coord x y) size used avail)))))
+        (let ((coord (make-coord x y)))
+          (extract-data
+            (cdr lines)
+            (cons (list coord size used avail) output)
+            (if (zerop used) coord empty-node)
+            (if (> size 500) (make-coord 0 y) hole-in-wall)
+            (if (> x x-max) x x-max)))))))
 
 (defun compare-a-and-b (used-a avail-a)
   (lambda (cnt b)
@@ -37,28 +44,33 @@
         (cdr nodes)
         :initial-value cnt))))
 
-(defun sort-by-coord (a b)
-  (destructuring-bind ((c _ _ _) . (d _ _ _)) (cons a b)
-    (let ((diff (- (get-y c) (get-y d))))
-      (cond
-        ((minusp diff) t)
-        ((zerop  diff) (< (get-x c) (get-x d)))))))
+;(defun sort-by-coord (a b)
+;  (destructuring-bind ((c _ _ _) . (d _ _ _)) (cons a b)
+;    (let ((diff (- (get-y c) (get-y d))))
+;      (cond
+;        ((minusp diff) t)
+;        ((zerop  diff) (< (get-x c) (get-x d)))))))
 
 (defun main ()
-  (let
-    ((input (cddr (read-input-as-list 22 #'decode))))
+  (destructuring-bind (input empty-node hole-in-wall x-max) (extract-data (cddr (read-input-as-list 22 )))
     (print (find-viable-pairs input))
-    (let
-      ((limits (get-coords-limits (mapcar #'car input)))
-       (sorted-input (sort input #'sort-by-coord)))
-      (terpri)
-      (iterate
-        (for y from 0 to (getf limits :y-max))
-        (iterate
-          (for x from 0 to (getf limits :x-max))
-          (destructuring-bind (_ capa used _) (pop input)
-            (if (> capa 500)
-              (format t " XXXXX")
-              (format t " ~2a/~2a" used capa))))
-        (format t "~%")))))
+    (print
+      (+
+        (manhattan-distance empty-node hole-in-wall)
+        (manhattan-distance hole-in-wall (make-coord (1- x-max) 0))
+        (1+ (* 5 (1- x-max)))))
+;    (let
+;      ((limits (get-coords-limits (mapcar #'car input)))
+;       (sorted-input (sort input #'sort-by-coord)))
+;      (terpri)
+;      (iterate
+;        (for y from 0 to (getf limits :y-max))
+;        (iterate
+;          (for x from 0 to (getf limits :x-max))
+;          (destructuring-bind (_ capa used _) (pop sorted-input)
+;            (if (> capa 500)
+;              (format t " XXXXX")
+;              (format t " ~2a/~2a" used capa))))
+;        (format t "~%")))
+    ))
 
