79687ceafda0ef854c9e2a0a514b91df93370f4e
Franck YVONNET <franck@yvonnet.fr>
2021-11-11 13:31:58 +0100

    externalized assembunny-related code

--- day12.lisp
+++ day12.lisp
@@ -1,60 +1,12 @@
 (defpackage :day12
   (:use :cl :aoc-misc)
   (:export main)
-  (:import-from :cl-ppcre :scan-to-strings)
-  (:import-from :serapeum :nlet)
-  (:import-from :trivia :match))
+  (:import-from :assembunny :decode-assembunny :run-assembunny))
 
 (in-package :day12)
 
-(defvar *regex* "^(\\w+) (\\S*)( (.+))?$")
-
-(defun decode-param (param)
-  (when param
-    (let ((first-char (char param 0)))
-      (if (alpha-char-p first-char)
-        (cons :register (- (char-code first-char) (char-code #\a)))
-        (cons :value (parse-integer param))))))
-
-(defun decode (line)
-  (multiple-value-bind (match reqs) (scan-to-strings *regex* line)
-    (match reqs
-      ((vector a b _ c)
-       (list 
-         (intern a "KEYWORD")
-         (decode-param b)
-         (decode-param c))))))
-
-(defun get-value (v regs)
-  (match v
-    ((cons :register x) (aref regs x))
-    ((cons :value x) x)))
-
-(defun run-program (prg ignition)
-  (let 
-    ((regs (make-array 4 :initial-element 0))
-     (len (length prg)))
-    (setf (aref regs 2) ignition)
-    (nlet rec ((ptr 0))
-      (if (>= ptr len)
-        (aref regs 0)
-        (match (aref prg ptr)
-          ((list :|cpy| x (cons :register r))
-           (setf (aref regs r) (get-value x regs))
-           (rec (1+ ptr)))
-          ((list :|inc| (cons :register r) nil)
-           (incf (aref regs r))
-           (rec (1+ ptr)))
-          ((list :|dec| (cons :register r) nil)
-           (decf (aref regs r))
-           (rec (1+ ptr)))
-          ((list :|jnz| x y)
-           (if (zerop (get-value x regs))
-             (rec (1+ ptr))
-             (rec (+ ptr (get-value y regs))))))))))
-
 (defun main ()
   (let
-    ((input (coerce (read-input-as-list 12 #'decode) 'vector)))
-    (dolist (ignition '(0 1)) (print (run-program input ignition)))))
+    ((input (coerce (read-input-as-list 12 #'decode-assembunny) 'vector)))
+    (dolist (ignition '(0 1)) (print (run-assembunny input ignition)))))
 
